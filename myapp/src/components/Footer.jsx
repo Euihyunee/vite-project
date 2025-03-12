d675fd5e91f52d9d701cd7501a15fcfc6f48f496
euihyunee <lobil7782@gmail.com>
2025-03-12 17:51:18 +0900

    [test] vite

---
+++ myapp/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+  return(<>
+    <h1>하단 영역</h1>
+    <div className="wrap">
+      <p>하단 영역입니다.</p>
+    </div>
+  </>)
+}
+
+export default Footer;
---
+++ myapp/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+  return (<>
+    <header>
+      <h1>헤더입니당</h1>
+      <div className="wrap">
+        <p>헤더영역입니다.</p>
+      </div>
+    </header>
+  </>)
+}
+
+export default Header
